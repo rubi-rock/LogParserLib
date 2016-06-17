5439483fc7ddd40bcb66d9d5720b2cc518d0031c
CROC <christian_rocher@HOTMAIL.COM>
2016-06-17 09:31:55 -0400

    Install from batch file

---
+++ install_requirements.bat
@@ -0,0 +1,5 @@
+cd install
+wget --no-check-certificate https://www.python.org/ftp/python/3.5.1/python-3.5.1-amd64.exe
+python-3.5.1-amd64.exe
+del python-3.5.1-amd64.exe
+call install_python_libs.bat
